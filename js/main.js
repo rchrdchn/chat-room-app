535f40721fb1cd652405f36382517569765f4c0d
Richard Chan <richardchanme@gmail.com>
2017-04-12 20:55:26 -0700

    adding files

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// EXERCISES
+
+// 1) Create a div in HTML, and style it using two classes - one
+// to define the height and width, the other to define background-color.
+// Then use JS to change that element's background-color.
+// When done with this, comment out your code.
+
+// function myColor() {
+// 	var changeNow = document.getElementById("change");
+// 	changeNow.className = "square color";
+// }
+
+// 2) Create a div in your HTML and give it an ID.
+// In JS, create an H1 and then use innerHTML to add some text to it.
+// Then add that H1 to the div in your HTML, still using JS.
+// When done with this, comment out your code.
+
+// function create() {
+// 	var text = document.createElement("div");
+// 	text.innerHTML = "Adding some more text";
+
+// 	var parent = document.getElementById("adding");
+// 	parent.appendChild(text);
+// }
+
+// 3) Create a div in your HTML and give it an ID.
+// In JS, create an H1. In JS, create a text node.
+// Then, add the text node to your H1, then add your H1 to your div in the HTML.
+// When done with this, comment out your code.
+
+// function noding(){
+// 	var create = document.createElement("h1");
+// 	var adding = document.createTextNode("writing here because I can do JS");
+// 	create.appendChild(adding);
+
+// 	var addNow = document.getElementById("node");
+
+// 	addNow.appendChild(create);
+// }
+
+// 4) Create an img and a button in your HTML.
+// In your JS, create a function that changes the image source when you
+// click on that button. When done with this, comment out your code.
+
+// function changeImg() {
+// 	var image = document.getElementById("imageMe");
+// 	image.src = "images/development3.jpg";
+// }
+
+
+
+
