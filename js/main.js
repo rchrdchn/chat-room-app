477b6561effa56f3e819fd5ee3f760cd9db978d3
Richard Chan <richardchanme@gmail.com>
2017-04-13 23:31:56 -0700

    working on logic for text to show up on browser

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-// EXERCISES
 
-// 1) Create a div in HTML, and style it using two classes - one
-// to define the height and width, the other to define background-color.
-// Then use JS to change that element's background-color.
-// When done with this, comment out your code.
-
-// function myColor() {
-// 	var changeNow = document.getElementById("change");
-// 	changeNow.className = "square color";
-// }
 
 // 2) Create a div in your HTML and give it an ID.
 // In JS, create an H1 and then use innerHTML to add some text to it.
@@ -23,30 +13,16 @@
 // 	parent.appendChild(text);
 // }
 
-// 3) Create a div in your HTML and give it an ID.
-// In JS, create an H1. In JS, create a text node.
-// Then, add the text node to your H1, then add your H1 to your div in the HTML.
-// When done with this, comment out your code.
+function messageMe() {
+	var write = document.getElementById("message").value.toLowerCase();
+	var div = document.createElement("div");
+	var addText = document.createElement("h3");
+	var addToBody = document.getElementById("body");
+	
+	write.innerHTML = "hey";
+	
+	console.log(write);
 
-// function noding(){
-// 	var create = document.createElement("h1");
-// 	var adding = document.createTextNode("writing here because I can do JS");
-// 	create.appendChild(adding);
-
-// 	var addNow = document.getElementById("node");
-
-// 	addNow.appendChild(create);
-// }
-
-// 4) Create an img and a button in your HTML.
-// In your JS, create a function that changes the image source when you
-// click on that button. When done with this, comment out your code.
-
-// function changeImg() {
-// 	var image = document.getElementById("imageMe");
-// 	image.src = "images/development3.jpg";
-// }
-
-
-
-
+	div.appendChild(addText);
+	body.appendChild(div);
+}
