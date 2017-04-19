5ced7feca6d8f153340594c26712b68b79c353dc
Richard Chan <richardchanme@gmail.com>
2017-04-18 17:44:11 -0700

    keypress event added

--- js/main.js
+++ js/main.js
@@ -11,3 +11,21 @@ function messageMe() {
 	div.className = "box-me";
 	addToBody.appendChild(div);
 }
+
+$("#message").on("keypress", function(e) {
+	if (e.which === 13) {
+		messageMe();
+	}
+});
+
+// function heartMe() {
+// 	var heart = document.getElementById("message").innerHTML = "<i class="fa fa-heart heart" aria-hidden="true"></i>";
+// 	var addHeart = document.createElement("h3");
+// 	var divHeart = document.createElement("div");
+// 	var addHeartToBody = document.getElementById("body");
+	
+// 	divHeart.appendChild(addHeart);
+// 	divHeart.appendChild(heart);
+// 	divHeart.className = "box-me";
+// 	addHeartToBody.appendChild(divHeart);	
+// }
