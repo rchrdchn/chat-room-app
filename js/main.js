14bf4e85ac2827528509fa2ef7b166770f67e062
Richard Chan <richardchanme@gmail.com>
2017-04-18 22:31:29 -0700

    final application

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
-// 
+// prompt question for name and adds name below <h1>
 
-function messageMe() {
-	var write = document.getElementById("message").value.toLowerCase();
-	var addText = document.createElement("h3");
-	var div = document.createElement("div");
-	var addToBody = document.getElementById("body");
-	
-	addText.innerHTML = write;
-	div.appendChild(addText);
-	div.className = "box-me";
-	addToBody.appendChild(div);
+var person = prompt("Please enter your name");
+	if (person != null) {
+    document.getElementById("username").innerHTML = "Welcome " + person + "!";
 }
 
+// function gets user input value on submit button
+function messageMe() {
+
+	// gets user input
+	var write = document.getElementById("message").value.toLowerCase();
+	// creates <h3> tag
+	var addText = document.createElement("h3");
+	// creates <div> tag
+	var div = document.createElement("div");
+	// targets ID body
+	var addToBody = document.getElementById("body");
+
+	// adds value input from write variable to DOM as <h3>, then spits user input value
+	addText.innerHTML = write;
+	// appends <h3> to <div>
+	div.appendChild(addText);
+	// adds "box-me" class to <div>
+	div.className = "box-me";
+	// appends <div> with <h3> in it to body ID, creating a box since class="box-me" has been added
+	addToBody.appendChild(div);
+
+	// if nothing in text input, do nothing
+	if (write.length == 0) {
+		div.className = "";
+		alert("Please type something :)");
+	};
+}
+
+// submits text on keypress (enter)
 $("#message").on("keypress", function(e) {
 	if (e.which === 13) {
 		messageMe();
+		this.val();
 	}
 });
 
-// function heartMe() {
-// 	var heart = document.getElementById("message").innerHTML = "<i class="fa fa-heart heart" aria-hidden="true"></i>";
-// 	var addHeart = document.createElement("h3");
-// 	var divHeart = document.createElement("div");
-// 	var addHeartToBody = document.getElementById("body");
-	
-// 	divHeart.appendChild(addHeart);
-// 	divHeart.appendChild(heart);
-// 	divHeart.className = "box-me";
-// 	addHeartToBody.appendChild(divHeart);	
-// }
